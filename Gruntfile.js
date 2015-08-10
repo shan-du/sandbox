3fac4f7c8d7f6cc1d1b8a07e07350ce2cace4af5
Shan Du <shan.du@nurun.com>
2015-08-10 18:53:01 -0400

    added grunt connect package & config

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,25 @@
 module.exports = function(grunt) {
 	
 	/**
+	 * Use built-in Node module (matchdep) to dynamically match & load package dependencies
+	 * https://github.com/tkellen/js-matchdep
+	 */
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	
+	/**
 	 * Set project info
 	 */
 	var project = {
-		imagePath: 'img',
+		imagePath: 'img/',
 		lessPath: 'less/',
 		cssPath: 'css/',
-		jsPath: 'js',
+		jsPath: 'js/',
 		minCss: 'css/styles.min.css',
-		watchFiles: ['less/*', 'css/*', 'js/*']
+		watchFiles: ['less/*', 'css/*', 'js/*'],
+		configFiles: ['Gruntfile.js']
 	};
+	
+	var LIVERELOAD_PORT = 1337;
 
 	grunt.initConfig({
 		
@@ -40,29 +49,52 @@ module.exports = function(grunt) {
 		 * Configuring Grunt tasks
 		 * http://gruntjs.com/configuring-tasks
 		 */
+		 
+		/** 
+		 * Local server via grunt-contrib-connect package
+		 */
+		connect: {
+			server: {
+				options: {
+					port: LIVERELOAD_PORT,
+					hostname: 'localhost'
+				}
+			}
+		},
+		
 		less: {
 			development: {
 				options: {
-					compress: true,
-					
+					compress: true
 				},
+				// Allows less/[filename].less to automatically compile to css/[filename].css
 				expand: true,
 				cwd: project.lessPath,
 				src: "*.less",
 				dest: project.cssPath,
 				ext: ".css",
-			},
+			}
 		},
 		
 		cssmin: {
 			production: {
 				files: {
-					project.minCss : ["css/*.css", "!" + project.minCss]
+					"<%= project.minCss %>" : ["css/*.css", "!<%= project.minCss %>"]
 				}
 			}
 		},
 		
 		watch: {
+			options: {
+				livereload: LIVERELOAD_PORT
+			},
+			// Enables grunt watch to reload upon config file changes
+			configFiles: {
+				files: project.configFiles,
+				options: {
+					reload: true
+				}
+			},
 			src: {
 				files: project.watchFiles,
 				tasks: ['default']
@@ -71,12 +103,14 @@ module.exports = function(grunt) {
 	});
 	
 	/**
-	 * Load plugins
+	 * Manual way of loading plugins
 	 * Remember to add these plugins to devDependencies in package.json
 	 */
+	/*
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	*/
 	
 	/**
 	 * Create/Define grunt tasks commands to run
@@ -88,10 +122,26 @@ module.exports = function(grunt) {
 	 *
 	 * http://gruntjs.com/creating-tasks
 	 */
-	grunt.registerTask('default', ['less']);
-	grunt.registerTask('release', ['less', 'cssmin:production']);
+	grunt.registerTask('default', [
+			'less'
+	]);
 	
+	grunt.registerTask('server', [
+			'connect',
+			'watch'
+	]);
+	
+	grunt.registerTask('release', [
+			'less', 
+			'cssmin:production'
+	]);
+	
+	/**
+	 * Add custom logic to a grunt task
+	 */
+	/*
 	grunt.event.on('watch', function(action, filepath) {
 		grunt.log.writeln(filepath + ' has been ' + action);
 	});
+	*/
 };
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# sandbox
+# Sandbox Dev Notes:
+## Install npm Packages & Dependencies
+```
+> npm install
+> npm install matchdep
+```
+
+## Start Local Dev
+```
+// Only watch for file changes
+> grunt watch
+
+// Start local f/e server
+> grunt server
+```
